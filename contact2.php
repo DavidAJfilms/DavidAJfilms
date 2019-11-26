c62ddbeb2035b9a4b78f9c7bc9d433259ab36e02
DavidAJfilms <57259446+DavidAJfilms@users.noreply.github.com>
2019-11-26 12:41:12 +0100

    Update contact2.php

--- contact2.php
+++ contact2.php
@@ -4,7 +4,7 @@ $field_name = $_POST['name'];
 $field_email = $_POST['email'];
 $field_message = $_POST['message'];
 
-$mailTo = " hola@davidajfilms.com "
+$mailTo = "hola@davidajfilms.com"
 $headers = "From: ".$fiel_email;
 $txt = "You have recieved an email from ".$name.".\n\n".$field_message;
 
